c432112850326a988c528104264df0e538b20556
Lloyd Livengood <lloyd-livengood@icloud.com>
2024-09-13 11:08:53 -0600

    leet speak

--- testGitGud/testGitGud/main.cpp
+++ testGitGud/testGitGud/main.cpp
@@ -10,6 +10,6 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Fingers crossed this works!\n";
-    std::cout << "And this code is super l33t \n";
+    std::cout << "And this code is super L33T, how do you do fellow kids? \n";
     return 0;
 }
