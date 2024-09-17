ffeff70a75fc10044afe523068d13504b050e3c0
Lloyd Livengood <lloyd-livengood@icloud.com>
2024-09-17 13:54:21 -0600

    added font, keyboard input for attacks, battle resolution, etc

--- src/character.hpp
+++ src/character.hpp
@@ -20,6 +20,7 @@ private:
     int DEF;
     int SPD;
     int shapeClass;
+    sf::Font font;
     
 public:
     Character(){                // Default Constructor
@@ -28,14 +29,16 @@ public:
         DEF = 1;
         SPD = 1;
         shapeClass = 1;
+        font.loadFromFile("ProtestGuerrilla-Regular.ttf");
     }
     
-    Character(int newHP, int newATK, int newDEF, int newSPD, int newShapeClass){        // Param'd constructor
+    Character(int newHP, int newATK, int newDEF, int newSPD, int newShapeClass) {        // Param'd constructor
         HP = newHP;
         ATK = newATK;
         DEF = newDEF;
         SPD = newSPD;
         shapeClass = newShapeClass;
+        font.loadFromFile("ProtestGuerrilla-Regular.ttf");
     }
     
     // Get methods for private stats
@@ -44,6 +47,7 @@ public:
     int getDEF() {return DEF;}
     int getSPD() {return SPD;}
     int getShapeClass() {return shapeClass;}
+    sf::Font getFont() {return font;}
   
     // Set methods for private stats
     void setHP(int newHP) {HP = newHP;}
@@ -55,6 +59,95 @@ public:
     sf::CircleShape render();               // Define a shape to be drawn representing the character
 
     // TODO WINDOW FOR PLAYER SHAPE STATS
+    sf::RectangleShape statsWindow(sf::RenderWindow &window) {
+        // Create the rectangle
+        sf::RectangleShape rectangle;
+        rectangle.setSize(sf::Vector2f(150, 100));
+        rectangle.setOutlineColor(sf::Color::Red);
+        rectangle.setOutlineThickness(3);
+        rectangle.setPosition(800, 650);
+
+        sf::Text HPLine;
+        std::string HPString = std::to_string(getHP());
+        HPLine.setFont(font); // Set the loaded font
+        HPLine.setString("HP: " + HPString); // Set the string to display
+        HPLine.setCharacterSize(20); // Set the text size
+        HPLine.setFillColor(sf::Color::Black); // Set the text color
+        HPLine.setPosition(rectangle.getPosition().x + 10, rectangle.getPosition().y + 10); // Position inside the rectangle
+        
+        sf::Text ATKLine;
+        std::string ATKString = std::to_string(getATK());
+        ATKLine.setFont(font);
+        ATKLine.setString("ATK: " + ATKString);
+        ATKLine.setCharacterSize(20);
+        ATKLine.setFillColor(sf::Color::Black);
+        ATKLine.setPosition(rectangle.getPosition().x + 10, rectangle.getPosition().y + 30);
+
+        sf::Text DEFLine;
+        std::string DEFString = std::to_string(getDEF());
+        DEFLine.setFont(font);
+        DEFLine.setString("DEF: " + DEFString);
+        DEFLine.setCharacterSize(20);
+        DEFLine.setFillColor(sf::Color::Black);
+        DEFLine.setPosition(rectangle.getPosition().x + 10, rectangle.getPosition().y + 50);
+        
+        sf::Text SPDLine;
+        std::string SPDString = std::to_string(getSPD());
+        SPDLine.setFont(font);
+        SPDLine.setString("SPD: " + SPDString);
+        SPDLine.setCharacterSize(20);
+        SPDLine.setFillColor(sf::Color::Black);
+        SPDLine.setPosition(rectangle.getPosition().x + 10, rectangle.getPosition().y + 70);
+
+        window.draw(rectangle);
+        window.draw(HPLine);
+        window.draw(ATKLine);
+        window.draw(DEFLine);
+        window.draw(SPDLine);
+        return rectangle;
+    }
+    
+    sf::RectangleShape actionWindow(sf::RenderWindow &window) {
+        // Create the rectangle
+        sf::RectangleShape rectangle;
+        rectangle.setSize(sf::Vector2f(150, 100));
+        rectangle.setOutlineColor(sf::Color::Blue);
+        rectangle.setOutlineThickness(3);
+        rectangle.setPosition(425, 650);
+        
+        sf::Text attack;
+        attack.setFont(font); // Set the loaded font
+        attack.setString("1. Attack" ); // Set the string to display
+        attack.setCharacterSize(20); // Set the text size
+        attack.setFillColor(sf::Color::Black); // Set the text color
+        attack.setPosition(rectangle.getPosition().x + 10, rectangle.getPosition().y + 10); // Position inside the rectangle
+        
+        sf::Text capture;
+        capture.setFont(font); // Set the loaded font
+        capture.setString("2. Capture" ); // Set the string to display
+        capture.setCharacterSize(20); // Set the text size
+        capture.setFillColor(sf::Color::Black); // Set the text color
+        capture.setPosition(rectangle.getPosition().x + 10, rectangle.getPosition().y + 30); // Position inside the rectangle
+        
+        sf::Text debuff10;
+        debuff10.setFont(font); // Set the loaded font
+        debuff10.setString("3. Debuff 10%" ); // Set the string to display
+        debuff10.setCharacterSize(20); // Set the text size
+        debuff10.setFillColor(sf::Color::Black); // Set the text color
+        debuff10.setPosition(rectangle.getPosition().x + 10, rectangle.getPosition().y + 50); // Position inside the rectangle
+
+        
+        
+        
+        window.draw(rectangle);
+        window.draw(attack);
+        window.draw(capture);
+        window.draw(debuff10);
+        return rectangle;
+    }
+    
+    
+    
     // TODO WINDOW FOR PLAYER ACTIONS
     // TODO WINDOW FOR ENEMY SHAPE STATS
     // TODO ENEMY GENERATOR, calculate stats based on duration of "run", higher randomized stats based on run duration
@@ -72,6 +165,29 @@ public:
     // TODO BATTLE PHASE
     
     // potential TODO of "resolution" depending on boolean states set by player/enemy hp
+    void attack(Character &rhs){
+        int damage = ATK - rhs.getDEF();
+        if (damage <1){
+            damage = 1;
+        }
+        rhs.setHP(rhs.getHP()-damage);
+    }
+    
+    void capture(Character &rhs){
+        int captureChance = (getHP()*getSPD()) / (rhs.getHP()*rhs.getSPD());
+        if( captureChance > (rand() % 100)) {
+            ATK += rhs.getATK();
+            DEF += rhs.getDEF();
+            SPD += rhs.getSPD();
+            shapeClass += rhs.getShapeClass();
+        }
+    }
+    
+    void debuff10(Character &rhs){
+        rhs.setATK(rhs.getATK()*.9);
+        rhs.setDEF(rhs.getDEF()*.9);
+        rhs.setSPD(rhs.getSPD()*.9);
+    }
     
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,42 +9,84 @@
 #include <cassert>
 #include "character.hpp"
 
-
 int main() {
-    testChar();
-    testSetGet();
+//    testChar();
+//    testSetGet();
+    sf::RenderWindow window (sf::VideoMode(1000,800), "Polymons");
+    int playerAction = 0;
+    std::srand(static_cast<unsigned>( int(time(nullptr))));
     
-    sf::RenderWindow window (sf::VideoMode(800,600), "MyFirstTactics");
+    Character player = Character(100,23,255,200,5);
+    Character enemy = Character(100,255,150,200,5);
     
     while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
+        
+        while (window.pollEvent(event)) {               // listening to key presses
             if (event.type == sf::Event::Closed)
                 window.close();
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Num1 ) {
+                std::cout << "player action 1" << std::endl;
+                playerAction = 1;
+            }
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Num2) {
+                std::cout << "player action 2" << std::endl;
+                playerAction = 2;
+            }
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Num3) {
+                std::cout << "player action 3" << std::endl;
+                playerAction = 3;
+            }
+         }
+        
+        window.clear(sf::Color::Black);                              // set black window
+
+        // TODO create player char on first, carry over on victory
+        sf::CircleShape displayPlayer = player.render();
+        displayPlayer.setPosition(20, 550);
+        window.draw(displayPlayer);
+        player.statsWindow(window);
+        player.actionWindow(window);
+        
+        
+        // generate enemy, use incremental random params for future rounds
+        sf::CircleShape displayEnemy = enemy.render();
+        displayEnemy.setPosition(600, 20);
+        window.draw(displayEnemy);
+            
+        if (playerAction != 0){                                     // Battle resolution
+            if(player.getSPD() - enemy.getSPD() >= 0){              // if the player is faster
+                switch (playerAction) {
+                    case 1:
+                        player.attack(enemy);
+                        std::cout << "HIYAAAAAAAAAAAAAAAA" << std::endl;
+                        break;
+                    case 2:
+                        player.capture(enemy);
+                        break;
+                    case 3:
+                        player.debuff10(enemy);
+                        break;
+                }
+                enemy.attack(player);
+            } else {                                                // the enemy is faster
+                enemy.attack(player);
+                switch (playerAction) {
+                    case 1:
+                        player.attack(enemy);
+                        std::cout << "HIYAAAAAAAAAAAAAAAA" << std::endl;
+                        break;
+                    case 2:
+                        player.capture(enemy);
+                        break;
+                    case 3:
+                        player.debuff10(enemy);
+                        break;
+                }
+            }
+            playerAction=0;
         }
-        
-//        sf::Time deltaTime = clock.restart();
-//        float dt = deltaTime.asSeconds();
-        
-        
-        window.clear(sf::Color::Black);
-        //call everything visual in between these two
-        
-        
-        Character testGuy = Character(100,23,255,200,5);
-        sf::CircleShape displayTestGuy = testGuy.render();
-        
-        
-        
-        
-        window.draw(displayTestGuy);
-        
-        // TODO TRIGGER PLAYER
-        
-        // TODO TRIGGER BATTLE PHASE (based on speed check)
-        
-        
-        
         
         
         
